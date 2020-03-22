77ac7de46f2ed4cd18065f89dd99c21346002a6f
Lassi Kortela <lassi@lassi.io>
2020-03-22 21:33:54 +0200

    Reformat with 4-space indents

--- test.c
+++ test.c
@@ -9,65 +9,71 @@
 
 #include "unix.h"
 
-static void usage(void) {
-  fprintf(stderr, "usage\n");
-  exit(2);
+static void usage(void)
+{
+    fprintf(stderr, "usage\n");
+    exit(2);
 }
 
-static void panic_errno(const char *which) {
-  fprintf(stderr, "%s: %s\n", which, strerror(errno));
-  exit(2);
+static void panic_errno(const char *which)
+{
+    fprintf(stderr, "%s: %s\n", which, strerror(errno));
+    exit(2);
 }
 
-static void do_getgroups(void) {
-  gid_t *gids;
-  size_t ngid, i;
+static void do_getgroups(void)
+{
+    gid_t *gids;
+    size_t ngid, i;
 
-  if (unix_getgroups(&gids, &ngid) == -1)
-    panic_errno("getcwd");
-  i = 0;
-  while (i < ngid - 1) {
-      printf("%ld ", (long)gids[i++]);
-  }
-  printf("%ld\n", (long)gids[i]);
-  free(gids);
+    if (unix_getgroups(&gids, &ngid) == -1)
+        panic_errno("getcwd");
+    i = 0;
+    while (i < ngid - 1) {
+        printf("%ld ", (long)gids[i++]);
+    }
+    printf("%ld\n", (long)gids[i]);
+    free(gids);
 }
 
-static void do_getcwd(void) {
-  char *dir;
-  if (unix_getcwd(&dir) == -1)
-    panic_errno("getcwd");
-  printf("%s\n", dir);
-  free(dir);
+static void do_getcwd(void)
+{
+    char *dir;
+    if (unix_getcwd(&dir) == -1)
+        panic_errno("getcwd");
+    printf("%s\n", dir);
+    free(dir);
 }
 
-static void do_readlink(const char *path) {
-  char *link;
-  if (unix_readlink(path, &link) == -1)
-    panic_errno("readlink");
-  printf("%s\n", link);
-  free(link);
+static void do_readlink(const char *path)
+{
+    char *link;
+    if (unix_readlink(path, &link) == -1)
+        panic_errno("readlink");
+    printf("%s\n", link);
+    free(link);
 }
 
-int main(int argc, char **argv) {
-  const char *which;
+int main(int argc, char **argv)
+{
+    const char *which;
 
-  if (argc < 2)
-    usage();
-  which = argv[1];
-  if (!strcmp(which, "getgroups")) {
-    if (argc != 2)
-      usage();
-    do_getgroups();
-  } else if (!strcmp(which, "getcwd")) {
-    if (argc != 2)
-      usage();
-    do_getcwd();
-  } else if (!strcmp(which, "readlink")) {
-    if (argc != 3)
-      usage();
-    do_readlink(argv[2]);
-  } else {
-    usage();
-  }
+    if (argc < 2)
+        usage();
+    which = argv[1];
+    if (!strcmp(which, "getgroups")) {
+        if (argc != 2)
+            usage();
+        do_getgroups();
+    } else if (!strcmp(which, "getcwd")) {
+        if (argc != 2)
+            usage();
+        do_getcwd();
+    } else if (!strcmp(which, "readlink")) {
+        if (argc != 3)
+            usage();
+        do_readlink(argv[2]);
+    } else {
+        usage();
+    }
 }
--- unix.c
+++ unix.c
@@ -12,143 +12,151 @@
 
 #include "unix.h"
 
-int unix_read(int fd, char *buf, size_t cap, size_t *out) {
-  size_t len;
+int unix_read(int fd, char *buf, size_t cap, size_t *out)
+{
+    size_t len;
 
-  while ((len = (size_t)read(fd, buf, cap)) == (size_t)-1) {
-    if (errno != EINTR) {
-      *out = 0;
-      return -1;
+    while ((len = (size_t)read(fd, buf, cap)) == (size_t)-1) {
+        if (errno != EINTR) {
+            *out = 0;
+            return -1;
+        }
     }
-  }
-  *out = len;
-  return 0;
+    *out = len;
+    return 0;
 }
 
-int unix_write(int fd, char *buf, size_t cap, size_t *out) {
-  size_t len;
+int unix_write(int fd, char *buf, size_t cap, size_t *out)
+{
+    size_t len;
 
-  while ((len = (size_t)write(fd, buf, cap)) == (size_t)-1) {
-    if (errno != EINTR) {
-      *out = 0;
-      return -1;
+    while ((len = (size_t)write(fd, buf, cap)) == (size_t)-1) {
+        if (errno != EINTR) {
+            *out = 0;
+            return -1;
+        }
     }
-  }
-  *out = len;
-  return 0;
+    *out = len;
+    return 0;
 }
 
-int unix_getgroups(gid_t **out_gids, size_t *out_ngid) {
-  gid_t *gids;
-  gid_t *newgids;
-  gid_t egid;
-  size_t cap, i;
-  int ngid, has_egid;
+int unix_getgroups(gid_t **out_gids, size_t *out_ngid)
+{
+    gid_t *gids;
+    gid_t *newgids;
+    gid_t egid;
+    size_t cap, i;
+    int ngid, has_egid;
 
-  egid = getegid();
-  gids = 0;
-  for (cap = 8 * sizeof(gid_t); cap <= 256 * sizeof(gid_t); cap *= 2) {
-    if (!(newgids = realloc(gids, cap)))
-      break;
-    gids = newgids;
-    ngid = getgroups((int)(cap / sizeof(gid_t)), gids);
-    if (ngid >= 0) {
-      i = has_egid = 0;
-      while (i < (size_t)ngid) {
-        if (gids[i++] == egid)
-          has_egid = 1;
-      }
-      if (!has_egid) {
-        if (i == cap / sizeof(gid_t))
-          continue;
-        gids[i++] = egid;
-        ngid++;
-      }
-      while (i < cap / sizeof(gid_t))
-        gids[i++] = (gid_t)-1;
-      *out_gids = gids;
-      *out_ngid = (size_t)ngid;
-      return 0;
+    egid = getegid();
+    gids = 0;
+    for (cap = 8 * sizeof(gid_t); cap <= 256 * sizeof(gid_t); cap *= 2) {
+        if (!(newgids = realloc(gids, cap)))
+            break;
+        gids = newgids;
+        ngid = getgroups((int)(cap / sizeof(gid_t)), gids);
+        if (ngid >= 0) {
+            i = has_egid = 0;
+            while (i < (size_t)ngid) {
+                if (gids[i++] == egid)
+                    has_egid = 1;
+            }
+            if (!has_egid) {
+                if (i == cap / sizeof(gid_t))
+                    continue;
+                gids[i++] = egid;
+                ngid++;
+            }
+            while (i < cap / sizeof(gid_t))
+                gids[i++] = (gid_t)-1;
+            *out_gids = gids;
+            *out_ngid = (size_t)ngid;
+            return 0;
+        }
     }
-  }
-  *out_gids = 0;
-  *out_ngid = 0;
-  return -1;
-}
-
-int unix_getcwd(char **out) {
-  char *buf;
-  char *newbuf;
-  size_t cap;
-  int err;
-
-  buf = 0;
-  for (cap = 64; cap != 0; cap *= 2) {
-    if (!(newbuf = realloc(buf, cap)))
-      break;
-    buf = newbuf;
-    if (getcwd(buf, cap)) {
-      *out = buf;
-      return 0;
-    }
-    if (errno != ERANGE)
-      break;
-  }
-  err = errno;
-  free(buf);
-  errno = err;
-  *out = 0;
-  return -1;
-}
-
-int unix_readlink(const char *path, char **out) {
-  char *buf;
-  char *newbuf;
-  size_t cap, len;
-  int err;
-
-  buf = 0;
-  for (cap = 64; cap != 0; cap *= 2) {
-    if (!(newbuf = realloc(buf, cap)))
-      break;
-    buf = newbuf;
-    len = (size_t)readlink(path, buf, cap);
-    if (len == (size_t)-1)
-      break;
-    if (len < cap) {
-      memset(buf + len, 0, cap - len);
-      *out = buf;
-      return 0;
-    }
-  }
-  err = errno;
-  free(buf);
-  errno = err;
-  *out = 0;
-  return -1;
-}
-
-static int fill_ipv4(struct sockaddr_in *addr, unsigned int ip,
-                     unsigned int port) {
-  memset(addr, 0, sizeof(*addr));
-  addr->sin_family = AF_INET;
-  addr->sin_port = htons(port);
-  addr->sin_addr.s_addr = htonl(ip);
-  return 0;
-}
-
-int unix_bind_ipv4(int fd, unsigned int ip, unsigned int port) {
-  struct sockaddr_in addr;
-
-  if (fill_ipv4(&addr, ip, port) == -1)
+    *out_gids = 0;
+    *out_ngid = 0;
     return -1;
-  return bind(fd, (struct sockaddr *)&addr, sizeof(addr));
 }
 
-int unix_connect_ipv4(int fd, unsigned int ip, unsigned int port) {
-  struct sockaddr_in addr;
+int unix_getcwd(char **out)
+{
+    char *buf;
+    char *newbuf;
+    size_t cap;
+    int err;
 
-  if (fill_ipv4(&addr, ip, port) == -1)
+    buf = 0;
+    for (cap = 64; cap != 0; cap *= 2) {
+        if (!(newbuf = realloc(buf, cap)))
+            break;
+        buf = newbuf;
+        if (getcwd(buf, cap)) {
+            *out = buf;
+            return 0;
+        }
+        if (errno != ERANGE)
+            break;
+    }
+    err = errno;
+    free(buf);
+    errno = err;
+    *out = 0;
     return -1;
-  return connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+}
+
+int unix_readlink(const char *path, char **out)
+{
+    char *buf;
+    char *newbuf;
+    size_t cap, len;
+    int err;
+
+    buf = 0;
+    for (cap = 64; cap != 0; cap *= 2) {
+        if (!(newbuf = realloc(buf, cap)))
+            break;
+        buf = newbuf;
+        len = (size_t)readlink(path, buf, cap);
+        if (len == (size_t)-1)
+            break;
+        if (len < cap) {
+            memset(buf + len, 0, cap - len);
+            *out = buf;
+            return 0;
+        }
+    }
+    err = errno;
+    free(buf);
+    errno = err;
+    *out = 0;
+    return -1;
+}
+
+static int fill_ipv4(
+    struct sockaddr_in *addr, unsigned int ip, unsigned int port)
+{
+    memset(addr, 0, sizeof(*addr));
+    addr->sin_family = AF_INET;
+    addr->sin_port = htons(port);
+    addr->sin_addr.s_addr = htonl(ip);
+    return 0;
+}
+
+int unix_bind_ipv4(int fd, unsigned int ip, unsigned int port)
+{
+    struct sockaddr_in addr;
+
+    if (fill_ipv4(&addr, ip, port) == -1)
+        return -1;
+    return bind(fd, (struct sockaddr *)&addr, sizeof(addr));
+}
+
+int unix_connect_ipv4(int fd, unsigned int ip, unsigned int port)
+{
+    struct sockaddr_in addr;
+
+    if (fill_ipv4(&addr, ip, port) == -1)
+        return -1;
+    return connect(fd, (struct sockaddr *)&addr, sizeof(addr));
 }
